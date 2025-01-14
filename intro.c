787af09474d91a22c43f759b2e4137614c595345
Jim Glenn <james.glenn@yale.edu>
2025-01-14 13:43:08 -0500

    Examples from first meeting

---
+++ intro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  char answer;
+  printf("Are you ready? ");
+  answer = getchar();
+  if (answer == 'y')
+    {
+      printf("Welcome to 223!\n");
+    }
+  else
+    {
+      printf("Maybe later...\n");
+    }
+  return 0;
+}
+
